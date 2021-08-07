a39b7d1b13a1bf1166bf401c8e6f19e85084b2da
Bogdan <dimoiubogdan03@gmail.com>
2021-08-07 12:17:02 +0300

    Done pawn

--- Pieces/Pawn.cs
+++ Pieces/Pawn.cs
@@ -4,8 +4,6 @@ namespace ChessGame.Pieces
 {
     public class Pawn : APiece
     {
-        public bool FirstMove = true;
-
         public Pawn(PieceColor color) : base(color, PieceType.Pawn)
         {
 
@@ -14,26 +12,28 @@ namespace ChessGame.Pieces
         public override List<Coordinate> GetAvailableMoves(Coordinate initialCoordinates, GameContext context)
         {
             List<Coordinate> availableMoves = new();
-            var isBlack = Color == PieceColor.Black;
+            bool isUp = Color == PieceColor.Black;
+            bool isFirstMove = isUp ? initialCoordinates.Y == 1 : initialCoordinates.Y == 6;
 
             if (context.ColorToMove == Color)
             {
-                if (FirstMove && isBlack ? initialCoordinates.Y + 2 <= 7 : initialCoordinates.Y - 2 <= 7 && isBlack ? initialCoordinates.Y + 2 >= 0 : initialCoordinates.Y - 2 >= 0)
+                // ceva ciudat la conditie ( ar trebui sa returneze false dupa prima mutare vezi isUp si precedenta operatorilor )
+                if (isFirstMove && !context.Layout.ContainsKey(Coordinate.GetInstance(initialCoordinates.X, isUp ? initialCoordinates.Y + 2 : initialCoordinates.Y - 2)) && !context.Layout.ContainsKey(Coordinate.GetInstance(initialCoordinates.X, isUp ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1)) && (isUp ? initialCoordinates.Y + 2 <= 7 : initialCoordinates.Y - 2 <= 7 && isUp ? initialCoordinates.Y + 2 >= 0 : initialCoordinates.Y - 2 >= 0))
                 {
-                    availableMoves.Add(Coordinate.GetInstance(initialCoordinates.X, isBlack ? initialCoordinates.Y + 2 : initialCoordinates.Y - 2));
+                    availableMoves.Add(Coordinate.GetInstance(initialCoordinates.X, isUp ? initialCoordinates.Y + 2 : initialCoordinates.Y - 2));
                 }
-                if (isBlack ? initialCoordinates.Y + 1 <= 7 : initialCoordinates.Y - 1 <= 7 && isBlack ? initialCoordinates.Y + 1 >= 0 : initialCoordinates.Y - 1 >= 0)
+                if (isUp ? initialCoordinates.Y + 1 <= 7 : initialCoordinates.Y - 1 <= 7 && isUp ? initialCoordinates.Y + 1 >= 0 : initialCoordinates.Y - 1 >= 0 && !context.Layout.ContainsKey(Coordinate.GetInstance(initialCoordinates.X, isUp ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1)))
                 {
-                    availableMoves.Add(Coordinate.GetInstance(initialCoordinates.X, isBlack ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1));
+                    availableMoves.Add(Coordinate.GetInstance(initialCoordinates.X, isUp ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1));
                 }
 
-                if (isBlack ? initialCoordinates.Y + 1 <= 7 : initialCoordinates.Y - 1 <= 7 && isBlack ? initialCoordinates.Y + 1 >= 0 : initialCoordinates.Y - 1 >= 0)
+                if (isUp ? initialCoordinates.Y + 1 <= 7 : initialCoordinates.Y - 1 <= 7 && isUp ? initialCoordinates.Y + 1 >= 0 : initialCoordinates.Y - 1 >= 0)
                 {
                     // atac stanga
                     if (initialCoordinates.X - 1 <= 7 && initialCoordinates.X - 1 >= 0)
                     {
-                        var LeftAttackCoordinate = Coordinate.GetInstance(initialCoordinates.X - 1, isBlack ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1);
-                        if (context.Layout.ContainsKey(LeftAttackCoordinate) && context.Layout[LeftAttackCoordinate] != null)
+                        var LeftAttackCoordinate = Coordinate.GetInstance(initialCoordinates.X - 1, isUp ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1);
+                        if (context.Layout.ContainsKey(LeftAttackCoordinate) && context.Layout[LeftAttackCoordinate] != null && context.Layout[LeftAttackCoordinate].Color != context.Layout[initialCoordinates].Color)
                         {
                             availableMoves.Add(LeftAttackCoordinate);
                         }
@@ -42,8 +42,8 @@ namespace ChessGame.Pieces
                     // atac dreapta
                     if (initialCoordinates.X + 1 <= 7 && initialCoordinates.X + 1 >= 0)
                     {
-                        var rightAttackCoordinate = Coordinate.GetInstance(initialCoordinates.X + 1, isBlack ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1);
-                        if (context.Layout.ContainsKey(rightAttackCoordinate) && context.Layout[rightAttackCoordinate] != null)
+                        var rightAttackCoordinate = Coordinate.GetInstance(initialCoordinates.X + 1, isUp ? initialCoordinates.Y + 1 : initialCoordinates.Y - 1);
+                        if (context.Layout.ContainsKey(rightAttackCoordinate) && context.Layout[rightAttackCoordinate] != null && context.Layout[rightAttackCoordinate].Color != context.Layout[initialCoordinates].Color)
                         {
                             availableMoves.Add(rightAttackCoordinate);
                         }
